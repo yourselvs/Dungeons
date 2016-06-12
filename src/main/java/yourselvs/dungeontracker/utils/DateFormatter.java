93b0c61c69762ead43cd70f63a667d12b6cfd21e
Michael <unconfigured@null.spigotmc.org>
2016-06-12 10:29:58 -0400

    Added additional utility method to DateFormatter

--- src/main/java/yourselvs/dungeontracker/utils/DateFormatter.java
+++ src/main/java/yourselvs/dungeontracker/utils/DateFormatter.java
@@ -1,6 +1,7 @@
 package yourselvs.dungeontracker.utils;
 
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class DateFormatter {
 	private static SimpleDateFormat longFormat;
@@ -25,4 +26,13 @@ public class DateFormatter {
      * @return	A formatter that formats dates in a short string.
      */
     public SimpleDateFormat getShortFormatter() {return shortFormat;}
+    
+    public Date subtractTime(Date date1, Date date2){
+		boolean negative = false;
+		long time1 = date1.getTime();
+		long time2 = date2.getTime();
+		long difference = time2 - time1;
+
+		return new Date(difference);
+	}
 }
