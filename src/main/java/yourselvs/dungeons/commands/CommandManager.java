dc445161ce1ec21abfc399b6e36ff1235e8772a1
Michael <unconfigured@null.spigotmc.org>
2016-06-14 13:54:04 -0400

    Updated CommandManager methods

--- src/main/java/yourselvs/dungeons/commands/CommandManager.java
+++ src/main/java/yourselvs/dungeons/commands/CommandManager.java
@@ -6,6 +6,8 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import yourselvs.dungeons.Dungeons;
+import yourselvs.dungeons.dungeons.Dungeon;
+import yourselvs.dungeons.dungeons.Dungeon.Difficulty;
 import yourselvs.dungeons.records.Record;
 
 public class CommandManager {
@@ -31,59 +33,59 @@ public class CommandManager {
 	
 	//}
 
-	public void viewLeaderboard(Player player, String dungeon, int page) {
+	public void viewLeaderboard(Player player, Dungeon dungeon, int page) {
 		
 	}
 	
-	public void getDungeonHistory(Player player, String dungeon){
+	public void getDungeonHistory(Player player, Dungeon dungeon){
 		List<Record> records = plugin.getRecordManager().getRecords(dungeon);
 	}
 	
-	public void getPlayerHistory(Player player, String dungeon, String playerName){
+	public void getPlayerHistory(Player player, Dungeon dungeon, String playerName){
 		
 	}
 	
-	public void getRecord(Player player, String dungeon){
+	public void getRecord(Player player, Dungeon dungeon){
 		
 	}
 	
-	public void getRecord(CommandSender sender, String dungeon){
+	public void getRecord(CommandSender sender, Dungeon dungeon){
 		
 	}
 	
-	public void getPlayerRecord(Player player, String dungeon, String playerName){
+	public void getPlayerRecord(Player player, Dungeon dungeon, String playerName){
 		
 	}
 	
-	public void getPlayerRecord(CommandSender sender, String dungeon, String playerName){
+	public void getPlayerRecord(CommandSender sender, Dungeon dungeon, String playerName){
 		
 	}
 	
-	public void createDungeon(Player sender, String dungeon, String difficulty, String creator){
+	public void createDungeon(Player sender, Dungeon dungeon, Difficulty difficulty, String creator){
 		
 	}
 	
-	public void deleteDungeon(Player sender, String dungeon){
+	public void deleteDungeon(Player sender, Dungeon dungeon){
 		
 	}
 	
-	public void setParam(Player player, String dungeon, String param, boolean bool){
+	public void setParam(Player player, Dungeon dungeon, String param, boolean bool){
 		
 	}
 	
-	public void getParam(Player player, String dungeon, String param){
+	public void getParam(Player player, Dungeon dungeon, String param){
 		
 	}
 	
-	public void allowCommand(Player player, String dungeon, String command){
+	public void allowCommand(Player player, Dungeon dungeon, String command){
 		
 	}
 	
-	public void blockCommand(Player player, String dungeon, String command){
+	public void blockCommand(Player player, Dungeon dungeon, String command){
 		
 	}
 	
-	public void getCommand(Player player, String dungeon, String command){
+	public void getCommand(Player player, Dungeon dungeon, String command){
 		
 	}
 	
