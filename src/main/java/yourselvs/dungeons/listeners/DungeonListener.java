74a0c41d1a5183cf9fddbb0ba469fe7ff27be310
Michael <unconfigured@null.spigotmc.org>
2016-06-14 23:21:36 -0400

    Added DungeonListener and updated PlayerListener
    Created DungeonListener, a listener that interacts with dungeon events.
    Added two new methods for the new dungeon events, and moved two dungeon
    event handlers from PlayerListener to DungeonListener to properly
    categorize them

---
+++ src/main/java/yourselvs/dungeons/listeners/DungeonListener.java
@@ -0,0 +1,52 @@
+package yourselvs.dungeons.listeners;
+
+import org.bukkit.event.Listener;
+
+import yourselvs.dungeons.Dungeons;
+import yourselvs.dungeons.events.DungeonPersonalRecordEvent;
+import yourselvs.dungeons.events.DungeonWorldRecordEvent;
+import yourselvs.dungeons.events.PlayerFinishDungeonEvent;
+import yourselvs.dungeons.events.PlayerStartDungeonEvent;
+import yourselvs.dungeons.records.Record;
+import yourselvs.dungeons.sessions.Session;
+
+public class DungeonListener implements Listener {
+	private Dungeons plugin;
+	
+	public DungeonListener(Dungeons instance) {
+		this.plugin = instance;
+		plugin.getServer().getPluginManager().registerEvents(this, plugin);
+	}
+	
+	public void onWorldRecord(DungeonWorldRecordEvent event) {
+		plugin.getMessenger().beatDungeonWR(event.getPreviousRecord(), event.getRecord());
+	}
+	
+	public void onPersonalRecord(DungeonPersonalRecordEvent event) {
+		plugin.getMessenger().beatDungeonPR(event.getPreviousRecord(), event.getRecord());
+	}
+	
+	public void onFinishDungeon(PlayerFinishDungeonEvent event) {		
+		Record record = new Record(event.getSession().getPlayer(), event.getSession().getDungeon(), event.getTime(), plugin.getFormatter().subtractTime(event.getTime(), event.getSession().getStart()));
+		plugin.getSessionManager().removeSession(record.getPlayer());
+		plugin.getRecordManager().addRecord(record);
+		
+		Record pr = plugin.getRecordManager().getFastestRecord(record.getDungeon(), record.getPlayer());
+		Record wr = plugin.getRecordManager().getFastestRecord(record.getDungeon());
+		
+		if(event.getTime().getTime() < pr.getTime().getTime()){
+			DungeonPersonalRecordEvent prEvent = new DungeonPersonalRecordEvent(record, pr);
+			plugin.getServer().getPluginManager().callEvent(prEvent);
+		}
+		if(event.getTime().getTime() < wr.getTime().getTime()){ // if the player beats the world record
+			DungeonWorldRecordEvent wrEvent = new DungeonWorldRecordEvent(record, wr);
+			plugin.getServer().getPluginManager().callEvent(wrEvent);
+		}
+	}
+	
+	public void onStartDungeon(PlayerStartDungeonEvent event) {
+		Session session = new Session(event.getPlayer().getUniqueId(), event.getDungeon(), event.getTime(), event.getPlayer().getLocation(), event.getPlayer().getInventory());
+		plugin.getSessionManager().addSession(session);
+		plugin.getMessenger().startDungeon(event.getPlayer(), plugin.getSessionManager().getSession(event.getPlayer()));
+	}
+}
--- src/main/java/yourselvs/dungeons/listeners/PlayerListener.java
+++ src/main/java/yourselvs/dungeons/listeners/PlayerListener.java
@@ -1,15 +1,10 @@
 package yourselvs.dungeons.listeners;
 
-import java.text.ParseException;
-import java.util.Date;
-
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 import yourselvs.dungeons.Dungeons;
-import yourselvs.dungeons.events.PlayerFinishDungeonEvent;
-import yourselvs.dungeons.records.Record;
 import yourselvs.dungeons.sessions.Session;
 
 
@@ -34,30 +29,5 @@ public class PlayerListener implements Listener{
 		
 	}
 	
-	public void onFinishDungeon(PlayerFinishDungeonEvent event) {		
-		Record record = new Record(event.getSession().getPlayer(), event.getSession().getDungeon(), event.getTime(), plugin.getFormatter().subtractTime(event.getTime(), event.getSession().getStart()));
-		plugin.getSessionManager().removeSession(record.getPlayer());
-		plugin.getRecordManager().addRecord(record);
-		
-		Record pr = plugin.getMongo().getFastestTime(event.getDungeon(), event.getPlayer());
-		Record wr = plugin.getMongo().getFastestTime(event.getDungeon());
-		
-		Date prDate = null;
-		Date wrDate = null;
-		try {
-			prDate = plugin.getFormatter().parse(plugin.subtractTime(pr.startTime, pr.finishTime));
-			wrDate = plugin.getFormatter().parse(plugin.subtractTime(wr.startTime, wr.finishTime));
-		} catch (ParseException e) {e.printStackTrace();}
-		
-		if(event.getTime().getTime() < prDate.getTime()) // if the player beats their personal record
-			plugin.getMessenger().beatDungeonPR(event.getPlayer(), pr, record);
-		
-		if(event.getTime().getTime() < wrDate.getTime()) // if the player beats the world record
-			plugin.getMessenger().beatDungeonWR(event.getPlayer(), wr, record);
-	}
 	
-	public void onStartDungeon(PlayerStartDungeonEvent event) {
-		plugin.getMongo().createRecord(event.getDungeon(), event.getPlayer());
-		plugin.getMessenger().startDungeon(event.getPlayer(), plugin.getMongo().getCurrentRecord(event.getPlayer()));
-	}
 }
