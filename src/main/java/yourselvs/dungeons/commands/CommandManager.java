3b23c4e612e8616ed3ab5fd31c2bd531963e7fdb
Michael Makogon <michael.makogon@gmail.com>
2016-08-02 00:14:04 -0400

    Added method shells to CommandManager

--- src/main/java/yourselvs/dungeons/commands/CommandManager.java
+++ src/main/java/yourselvs/dungeons/commands/CommandManager.java
@@ -1,6 +1,11 @@
 package yourselvs.dungeons.commands;
 
+import org.bukkit.entity.Player;
+
 import yourselvs.dungeons.Dungeons;
+import yourselvs.dungeons.dungeons.Dungeon;
+import yourselvs.dungeons.dungeons.Dungeon.Difficulty;
+import yourselvs.dungeons.sessions.Session;
 
 public class CommandManager {
 	private Dungeons plugin;
@@ -13,7 +18,70 @@ public class CommandManager {
 		this.plugin = instance;
 	}
 	
+	public void joinDungeon(Player player, Dungeon dungeon){
+		
+		player.teleport(dungeon.getStart());
+	}
 	
+	public void leaveDungeon(Player player){
+		
+	}
+	
+	public void completeDungeon(Session session){
+		
+	}
+	
+	public void viewTop(Player player, Dungeon dungeon){
+		
+	}
+	
+	public void viewHistory(Player player, Player target, Dungeon dungeon){
+		
+	}
+	
+	public void viewDungeonHistory(Player player, Dungeon dungeon){
+		
+	}
+	
+	public void viewPlayerHistory(Player player, Player target){
+		
+	}
+	
+	public void viewRecord(Player player, Dungeon dungeon){
+		
+	}
+	
+	public void viewPlayerRecord(Player player, Player target, Dungeon dungeon){
+		
+	}
+	
+	public void viewRank(Player player, Dungeon dungeon){
+		
+	}
+	
+	public void createDungeon(Player player, String dungeon, Difficulty difficulty, String creator){
+		
+	}
+	
+	public void deleteDungeon(Player player, Dungeon dungeon){
+		
+	}
+	
+	public void viewCommand(Dungeon dungeon, String command){
+		
+	}
+	
+	public void setCommand(Dungeon dungeon, String command, boolean value){
+		
+	}
+	
+	public void viewDungeon(Player player, Dungeon dungeon){
+		
+	}
+	
+	public void viewInfo(Player player){
+		
+	}
 	
 	// TODO Build command manager
 }
