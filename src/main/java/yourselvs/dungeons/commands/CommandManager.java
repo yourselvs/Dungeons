12e01af065754121fbd69ede01d9ac456d310123
Michael Makogon <michael.makogon@gmail.com>
2016-08-04 16:04:13 -0400

    Created release version of CommandManager

--- src/main/java/yourselvs/dungeons/commands/CommandManager.java
+++ src/main/java/yourselvs/dungeons/commands/CommandManager.java
@@ -1,10 +1,19 @@
 package yourselvs.dungeons.commands;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 import yourselvs.dungeons.Dungeons;
 import yourselvs.dungeons.dungeons.Dungeon;
 import yourselvs.dungeons.dungeons.Dungeon.Difficulty;
+import yourselvs.dungeons.events.PlayerFinishDungeonEvent;
+import yourselvs.dungeons.events.PlayerLeaveDungeonEvent;
+import yourselvs.dungeons.events.PlayerStartDungeonEvent;
 import yourselvs.dungeons.sessions.Session;
 
 public class CommandManager {
@@ -18,70 +27,131 @@ public class CommandManager {
 		this.plugin = instance;
 	}
 	
+	public void viewHelp(Player player) {
+		List<String> msgs = new ArrayList<String>();
+		
+		msgs.add("Available commands:");
+		if(player.hasPermission("dungeon.info"))
+			msgs.add(ChatColor.YELLOW + "/dungeon" + ChatColor.RESET + " : View information about the plugin");
+		if(player.hasPermission("dungeon.join")){
+			msgs.add(ChatColor.YELLOW + "/dungeon list" + ChatColor.RESET + " : List the dungeons you are able to join");
+			msgs.add(ChatColor.YELLOW + "/dungeon join [dungeon]" + ChatColor.RESET + " : Joins a dungeon");
+			msgs.add(ChatColor.YELLOW + "/dungeon leave" + ChatColor.RESET + " : Leaves the dungeon you are in");
+		}
+		if(player.hasPermission("dungeon.forcejoin"))
+			msgs.add(ChatColor.YELLOW + "/dungeon forcejoin [player] [dungeon]" + ChatColor.RESET + " : Forces a player to join a dungeon");
+		if(player.hasPermission("dungeon.forceleave"))
+			msgs.add(ChatColor.YELLOW + "/dungeon forceleave [player]" + ChatColor.RESET + " : Forces a player to leave a dungeon");
+		if(player.hasPermission("dungeon.create")){
+			msgs.add(ChatColor.YELLOW + "/dungeon create [dungeon] [difficulty] [creator]" + ChatColor.RESET + " : Creates a dungeon with a spawn at the point you are standing");
+			msgs.add(ChatColor.YELLOW + "/dungeon delete [dungeon]" + ChatColor.RESET + " : Deletes a dungeon");
+		}
+		
+		plugin.getMessenger().sendMessages(player, msgs);
+	}
+	
+	public void listDungeons(Player player) {
+		List<String> msgs = new ArrayList<String>();
+		
+		msgs.add("Dungeons available:");
+		for(Dungeon dungeon : plugin.getDungeonManager().getDungeons()){
+			Difficulty difficulty = dungeon.getDifficulty();
+			
+			String color = "";
+			if(difficulty == Difficulty.EASY)
+				color = "" + ChatColor.GREEN;
+			else if(difficulty == Difficulty.MEDIUM)
+				color = "" + ChatColor.GOLD;
+			else if(difficulty == Difficulty.HARD)
+				color = "" + ChatColor.RED;
+			else if(difficulty == Difficulty.INSANE)
+				color = "" + ChatColor.DARK_RED;
+				
+			msgs.add(ChatColor.YELLOW + dungeon.getName() + ChatColor.RESET + " : " + color + dungeon.getDifficulty().toString() + ChatColor.RESET + " : Created by " + ChatColor.YELLOW + dungeon.getCreator());
+		}
+		
+		msgs.add("" + ChatColor.RED + ChatColor.BOLD + "WARNING: " + ChatColor.RED + "Your inventory, armor, and potion effects are cleared upon joining a dungeon.");
+		
+		plugin.getMessenger().sendMessages(player, msgs);
+	}
+	
 	public void joinDungeon(Player player, Dungeon dungeon){
-		
-		player.teleport(dungeon.getStart());
+		PlayerStartDungeonEvent event = new PlayerStartDungeonEvent(player, new Date(), dungeon);
+		Bukkit.getServer().getPluginManager().callEvent(event);
+		if(!event.isCancelled())
+			player.teleport(dungeon.getStart());
+		player.setFlying(false);
+		player.getInventory().clear();
+		plugin.getMessenger().sendMessage(player, "Joining dungeon: " + ChatColor.YELLOW + dungeon.getName());
 	}
 	
-	public void leaveDungeon(Player player){
-		
+	public void leaveDungeon(Player player, Session session){
+		PlayerLeaveDungeonEvent event = new PlayerLeaveDungeonEvent(player, session.getDungeon());
+		Bukkit.getServer().getPluginManager().callEvent(event);
+		if(!event.isCancelled())
+			player.teleport(session.getLocation());
+		plugin.getMessenger().sendMessage(player, "Leaving dungeon: " + ChatColor.YELLOW + session.getDungeon().getName());
 	}
 	
-	public void completeDungeon(Session session){
-		
+	public void completeDungeon(Player player, Session session){
+		PlayerFinishDungeonEvent event = new PlayerFinishDungeonEvent(session, new Date());
+		Bukkit.getServer().getPluginManager().callEvent(event);
+		if(!event.isCancelled())
+			player.teleport(session.getLocation());
 	}
 	
 	public void viewTop(Player player, Dungeon dungeon){
-		
+		plugin.getMessenger().sendMessage(player, "This feature is not available yet.");
 	}
 	
 	public void viewHistory(Player player, Player target, Dungeon dungeon){
-		
+		plugin.getMessenger().sendMessage(player, "This feature is not available yet.");
 	}
 	
 	public void viewDungeonHistory(Player player, Dungeon dungeon){
-		
+		plugin.getMessenger().sendMessage(player, "This feature is not available yet.");
 	}
 	
 	public void viewPlayerHistory(Player player, Player target){
-		
+		plugin.getMessenger().sendMessage(player, "This feature is not available yet.");
 	}
 	
 	public void viewRecord(Player player, Dungeon dungeon){
-		
+		plugin.getMessenger().sendMessage(player, "This feature is not available yet.");
 	}
 	
 	public void viewPlayerRecord(Player player, Player target, Dungeon dungeon){
-		
+		plugin.getMessenger().sendMessage(player, "This feature is not available yet.");
 	}
 	
 	public void viewRank(Player player, Dungeon dungeon){
-		
+		plugin.getMessenger().sendMessage(player, "This feature is not available yet.");
 	}
 	
-	public void createDungeon(Player player, String dungeon, Difficulty difficulty, String creator){
-		
+	public void createDungeon(Player player, String name, Difficulty difficulty, String creator){
+		Dungeon dungeon = new Dungeon(name, player.getLocation(), creator, difficulty);
+		plugin.getDungeonManager().addDungeon(dungeon);
+		plugin.getMessenger().sendMessage(player, "Dungeon created: " + ChatColor.YELLOW + name);
 	}
 	
 	public void deleteDungeon(Player player, Dungeon dungeon){
-		
+		plugin.getDungeonManager().removeDungeon(dungeon.getName());
 	}
 	
-	public void viewCommand(Dungeon dungeon, String command){
-		
+	public void viewCommand(Player player, Dungeon dungeon, String command){
+		plugin.getMessenger().sendMessage(player, "This feature is not available yet.");
 	}
 	
-	public void setCommand(Dungeon dungeon, String command, boolean value){
-		
+	public void setCommand(Player player, Dungeon dungeon, String command, boolean value){
+		plugin.getMessenger().sendMessage(player, "This feature is not available yet.");
 	}
 	
 	public void viewDungeon(Player player, Dungeon dungeon){
-		
+		plugin.getMessenger().sendMessage(player, "This feature is not available yet.");
 	}
 	
 	public void viewInfo(Player player){
-		
+		plugin.getMessenger().sendMessage(player, "Dungeons plugin v" + ChatColor.YELLOW + plugin.version);
+		plugin.getMessenger().sendMessage(player, "Created by " + ChatColor.YELLOW + plugin.creator);
 	}
-	
-	// TODO Build command manager
 }
