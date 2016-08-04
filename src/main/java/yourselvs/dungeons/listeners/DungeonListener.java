f802f9e0a4c333e6a70a19b2b67056eccf750279
Michael Makogon <michael.makogon@gmail.com>
2016-08-04 16:11:21 -0400

    Fixed numerous bugs in DungeonListener
    The bugs caused several things to do with records from properly getting
    tracked and caused bugs with Sessions not getting deleted.

--- src/main/java/yourselvs/dungeons/listeners/DungeonListener.java
+++ src/main/java/yourselvs/dungeons/listeners/DungeonListener.java
@@ -1,12 +1,21 @@
 package yourselvs.dungeons.listeners;
 
+import java.util.ArrayList;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 import yourselvs.dungeons.Dungeons;
 import yourselvs.dungeons.events.DungeonPersonalRecordEvent;
 import yourselvs.dungeons.events.DungeonWorldRecordEvent;
 import yourselvs.dungeons.events.PlayerFinishDungeonEvent;
+import yourselvs.dungeons.events.PlayerLeaveDungeonEvent;
 import yourselvs.dungeons.events.PlayerStartDungeonEvent;
 import yourselvs.dungeons.records.Record;
 import yourselvs.dungeons.sessions.Session;
@@ -17,45 +26,78 @@ public class DungeonListener implements Listener {
 	public DungeonListener(Dungeons instance) {
 		this.plugin = instance;
 		plugin.getServer().getPluginManager().registerEvents(this, plugin);
+		plugin.getMessenger().sendServerMessage("Dungeon listener enabled");
 	}
 	
+	@EventHandler
 	public void onWorldRecord(DungeonWorldRecordEvent event) {
-		plugin.getMessenger().beatDungeonWR(event.getPreviousRecord(), event.getRecord());
+		ArrayList<String> messages = new ArrayList<String>();
+		messages.add(ChatColor.DARK_PURPLE + "*** NEW WORLD RECORD ***");
+		messages.add("" + plugin.getServer().getPlayer(event.getRecord().getPlayer()).getName() + " beat the world record in " + ChatColor.YELLOW + event.getRecord().getDungeon().getName() + ChatColor.RESET + " with a time of " + ChatColor.YELLOW + plugin.getFormatter().getShortFormatter().format(event.getRecord().getTime()) + ChatColor.RESET + ", beating the old time by " + ChatColor.YELLOW + plugin.getFormatter().getShortFormatter().format(plugin.getFormatter().subtractTime(event.getPreviousRecord().getTime(), event.getRecord().getTime())) + ChatColor.RESET + ".");
+		plugin.getMessenger().sendServerMessage(messages);
 	}
 	
+	@EventHandler
 	public void onPersonalRecord(DungeonPersonalRecordEvent event) {
-		plugin.getMessenger().beatDungeonPR(event.getPreviousRecord(), event.getRecord());
+		plugin.getMessenger().sendMessage(plugin.getServer().getPlayer(event.getRecord().getPlayer()), "You beat your personal record in " + ChatColor.YELLOW + event.getRecord().getDungeon().getName() + ChatColor.RESET + " by " + ChatColor.YELLOW + plugin.getFormatter().getShortFormatter().format(plugin.getFormatter().subtractTime(event.getPreviousRecord().getTime(), event.getRecord().getTime())) + ChatColor.RESET + ".");
 	}
 	
-	public void onFinishDungeon(PlayerFinishDungeonEvent event) {		
+	@EventHandler
+	public void onStartDungeon(PlayerStartDungeonEvent event) {
+		Session session = new Session(event.getPlayer().getUniqueId(), event.getDungeon(), event.getTime(), event.getPlayer().getLocation());
+		plugin.getSessionManager().addSession(session);
+	}
+	
+	@EventHandler
+	public void onFinishDungeon(PlayerFinishDungeonEvent event) {	
 		Record record = new Record(event.getSession().getPlayer(), event.getSession().getDungeon(), event.getTime(), plugin.getFormatter().subtractTime(event.getTime(), event.getSession().getStart()));
-		plugin.getSessionManager().removeSession(record.getPlayer());
-		plugin.getRecordManager().addRecord(record);
-		
 		Record pr = plugin.getRecordManager().getFastestRecord(record.getDungeon(), record.getPlayer());
 		Record wr = plugin.getRecordManager().getFastestRecord(record.getDungeon());
 		
-		if(event.getTime().getTime() < pr.getTime().getTime()){
+		plugin.getSessionManager().removeSession(record.getPlayer());
+		plugin.getRecordManager().addRecord(record);
+		Player player = plugin.getServer().getPlayer(record.getPlayer());
+		plugin.getMessenger().sendMessage(player, "You finished " + ChatColor.YELLOW + record.getDungeon().getName() + ChatColor.RESET + " with a time of " + ChatColor.YELLOW + plugin.getFormatter().getShortFormatter().format(record.getTime()));
+		
+		String prTime = "N/A";
+		String wrTime = "N/A";
+		
+		if(pr != null)
+			prTime = plugin.getFormatter().getShortFormatter().format(pr.getTime());
+		if(wr != null)
+			wrTime = plugin.getFormatter().getShortFormatter().format(wr.getTime());
+		
+		plugin.getMessenger().sendMessage(player, "PR time: " + ChatColor.YELLOW + prTime);
+		plugin.getMessenger().sendMessage(player, "WR time: " + ChatColor.YELLOW + wrTime);
+		
+		if(pr == null ||(record.getTime().getTime() < pr.getTime().getTime())){
 			DungeonPersonalRecordEvent prEvent = new DungeonPersonalRecordEvent(record, pr);
 			plugin.getServer().getPluginManager().callEvent(prEvent);
 		}
-		if(event.getTime().getTime() < wr.getTime().getTime()){ // if the player beats the world record
+		if(wr != null ||(record.getTime().getTime() < wr.getTime().getTime())){ // if the player beats the world record
 			DungeonWorldRecordEvent wrEvent = new DungeonWorldRecordEvent(record, wr);
 			plugin.getServer().getPluginManager().callEvent(wrEvent);
 		}
 	}
 	
+	@EventHandler
+	public void onLeaveDungeon(PlayerLeaveDungeonEvent event) {
+		plugin.getSessionManager().removeSession(event.getPlayer().getUniqueId());
+	}
+	
+	@EventHandler
 	public void onPlayerQuit(PlayerQuitEvent event) {
 		Session session = plugin.getSessionManager().getSession(event.getPlayer());
 		if(session != null){
 			plugin.getSessionManager().removeSession(session.getPlayer());
-			plugin.getMessenger().quitDungeon(event.getPlayer(), session);
 		}
 	}
 	
-	public void onStartDungeon(PlayerStartDungeonEvent event) {
-		Session session = new Session(event.getPlayer().getUniqueId(), event.getDungeon(), event.getTime(), event.getPlayer().getLocation(), event.getPlayer().getInventory());
-		plugin.getSessionManager().addSession(session);
-		plugin.getMessenger().startDungeon(event.getPlayer(), plugin.getSessionManager().getSession(event.getPlayer()));
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onPlayerDeath(PlayerRespawnEvent event) {
+		Session session = plugin.getSessionManager().getSession(event.getPlayer());
+		if(session != null){
+			event.setRespawnLocation(session.getDungeon().getStart());
+		}
 	}
 }
