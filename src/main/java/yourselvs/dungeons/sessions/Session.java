01e4702695a5a865f91767798fc865180176a119
Michael Makogon <michael.makogon@gmail.com>
2016-08-04 16:07:52 -0400

    Removed inventory from Session

--- src/main/java/yourselvs/dungeons/sessions/Session.java
+++ src/main/java/yourselvs/dungeons/sessions/Session.java
@@ -4,8 +4,6 @@ import java.util.Date;
 import java.util.UUID;
 
 import org.bukkit.Location;
-import org.bukkit.inventory.Inventory;
-
 import yourselvs.dungeons.dungeons.Dungeon;
 
 public class Session {
@@ -13,7 +11,6 @@ public class Session {
 	private Dungeon dungeon;
 	private Date start;
 	private Location location;
-	private Inventory inventory;
 	
 	/**
 	 * An object that stores information on a player currently in a dungeon.
@@ -22,19 +19,16 @@ public class Session {
 	 * @param dungeon	The dungeon the player is in.
 	 * @param start		The time the player started.
 	 * @param location	The location the player was in upon staring the dungeon
-	 * @param inventory	The inventory the player had upon starting the dungeon
 	 */
-	public Session(UUID player, Dungeon dungeon, Date start, Location location, Inventory inventory){
+	public Session(UUID player, Dungeon dungeon, Date start, Location location){
 		this.player = player;
 		this.dungeon = dungeon;
 		this.start = start;
 		this.location = location;
-		this.inventory = inventory;
 	}
 	
 	public UUID getPlayer() {return player;}
 	public Dungeon getDungeon() {return dungeon;}
 	public Date getStart() {return start;}
 	public Location getLocation() {return location;}
-	public Inventory getInventory() {return inventory;}
 }
