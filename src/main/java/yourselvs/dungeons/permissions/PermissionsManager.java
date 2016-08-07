8832801e1295a4c79b0eae94ed1e60dcdfde1d2e
Michael Makogon <yourselvs@live.com>
2016-08-07 15:39:45 -0400

    Updated PermissionsManager
    Removed obsolete functionality and modified/added some code for future
    implementation.

--- src/main/java/yourselvs/dungeons/permissions/PermissionsManager.java
+++ src/main/java/yourselvs/dungeons/permissions/PermissionsManager.java
@@ -1,7 +1,9 @@
 package yourselvs.dungeons.permissions;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.bukkit.permissions.Permission;
 import org.bukkit.plugin.PluginManager;
@@ -11,43 +13,23 @@ import yourselvs.dungeons.Dungeons;
 public class PermissionsManager {
 	private Dungeons plugin;
 	private PluginManager pluginManager;
-	private List<Permission> permissions = new ArrayList<Permission>();
-	private boolean loaded = false;
+	private Set<String> permissions = new HashSet<String>();
 	
 	public PermissionsManager(Dungeons instance){
 		this.plugin = instance;
 		this.pluginManager = plugin.getServer().getPluginManager();
-		
-		permissions.add(new Permission("dungeon.info")); // view info about the plugin and each dungeon
-		permissions.add(new Permission("dungeon.join")); // join/leave a dungeon
-		permissions.add(new Permission("dungeon.forcejoin"));; // force a player to join/leave a dungeon
-		permissions.add(new Permission("dungeon.top")); // view the leaderboards of a dungeon
-		permissions.add(new Permission("dungeon.history")); // view the completion history of a dungeon or a specific player in a dungeon
-		permissions.add(new Permission("dungeon.record")); // view dungeon world record, personal record, and other players records
-		permissions.add(new Permission("dungeon.rank")); // view your rank in a dungeon
-		permissions.add(new Permission("dungeon.create")); // create a dungeon
-		permissions.add(new Permission("dungeon.command")); // view/change a commmand value of a dungeon
 	}
 	
-	public List<Permission> getPermissions(){return permissions;}
-	
-	public void addPermission(Permission permission){
-		permissions.add(permission);
-		pluginManager.addPermission(permission);
-	}
+	public Set<String> getPermissions(){return permissions;}
 	
 	public void addPermission(String permissionStr){
 		Permission permission = new Permission(permissionStr);
-		permissions.add(permission);
+		permissions.add(permissionStr);
 		pluginManager.addPermission(permission);
 	}
 	
-	public void loadPermissions(){
-		if(!loaded){			
-			for(Permission permission : permissions)
-				pluginManager.addPermission(permission);
-			
-			loaded = true;
-		}
+	public void removePermission(String permission){
+		permissions.remove(permission);
+		pluginManager.removePermission(permission);
 	}
 }
