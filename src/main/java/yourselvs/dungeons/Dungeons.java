38d9edfb3fb2385ceb0f222d0784ce523d385468
Michael Makogon <yourselvs@live.com>
2016-08-07 23:00:48 -0400

    Organized imports

--- src/main/java/yourselvs/dungeons/Dungeons.java
+++ src/main/java/yourselvs/dungeons/Dungeons.java
@@ -6,7 +6,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import yourselvs.dungeons.commands.Cmd;
